496ac7da2583520c9545346d0e75c18d117f8146
Szymon Kędzior <szymon.ked16@gmail.com>
2024-11-11 12:35:09 +0100

    Add initializeBoard function to display the borad

--- Statki/main.cpp
+++ Statki/main.cpp
@@ -6,7 +6,13 @@
 #define SIZE 10
 
 // Inicjacja planszy pustymi znakami '.', reprezentujące puste pola
-void initializeBoard(char borad[SIZE][SIZE]);
+void initializeBoard(char borad[SIZE][SIZE]) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			borad[i][j] = '.';
+		}
+	}
+};
 // Drukowanie planszy na ekranie
 void printBoard(char board[SIZE][SIZE]);
 
