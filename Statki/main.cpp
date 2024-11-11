c21f9962f13cf1bd473b0a810080afb52e77d33f
Szymon Kędzior <szymon.ked16@gmail.com>
2024-11-11 12:36:47 +0100

    Add printBoard function to display the board

--- Statki/main.cpp
+++ Statki/main.cpp
@@ -14,7 +14,14 @@ void initializeBoard(char borad[SIZE][SIZE]) {
 	}
 };
 // Drukowanie planszy na ekranie
-void printBoard(char board[SIZE][SIZE]);
+void printBoard(char board[SIZE][SIZE]) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+};
 
 int main() {
 	char board[SIZE][SIZE];
