d74a623e3d264d5c014333f0adfda7fc344fdd69
Szymon Kędzior <szymon.ked16@gmail.com>
2024-11-11 12:28:27 +0100

    Dodaj pliki projektów.

---
+++ Statki/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Stała definiująca rozmiar planszy
+#define SIZE 10
+
+// Inicjacja planszy pustymi znakami '.', reprezentujące puste pola
+void initializeBoard(char borad[SIZE][SIZE]);
+// Drukowanie planszy na ekranie
+void printBoard(char board[SIZE][SIZE]);
+
+int main() {
+	char board[SIZE][SIZE];
+	srand(time(NULL));
+
+	initializeBoard(board);
+	printBoard(board);
+
+	return 0;
+}
